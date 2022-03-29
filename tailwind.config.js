5fc4e38454e5d28fd4f5ac733e2be2f46eb7c59c
Wen-Lin Lee <82gogolee@gmail.com>
2022-03-29 14:16:28 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#96CEB4',
+        secondary: {
+          100: '#FFEEAD',
+          200: '#FFAD60',
+        },
+      },
+      fontFamily: {
+        body: ['Nunito'],
+      },
+    },
+  },
+  plugins: [],
+};
